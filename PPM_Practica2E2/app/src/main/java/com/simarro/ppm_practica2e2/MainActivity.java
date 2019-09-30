caef47820c3a94a892f7050649cdd182c97a0a42
Edgar <edgaralkugm@gmail.com>
2019-09-30 10:44:46 +0200

    Ejercicio 2 acabado

--- PPM_Practica2E2/app/src/main/java/com/simarro/ppm_practica2e2/MainActivity.java
+++ PPM_Practica2E2/app/src/main/java/com/simarro/ppm_practica2e2/MainActivity.java
@@ -1,14 +1,56 @@
 package com.simarro.ppm_practica2e2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boton = findViewById(R.id.button);
+        boton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED){
+            contactos();
+
+        }else{
+            Utilidades.solicitarPermiso(Manifest.permission.READ_CONTACTS,"La app necesita permiso para leer contactos.",1,this);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions, int[] grantResults){
+        if (requestCode == 1){
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                Log.d("Acceso","Se ha accedido a los contactos");
+            }else{
+                Log.d("Acceso","Sin permiso no se puede acceder a contactos");
+            }
+        }
+    }
+
+    public void contactos(){
+        Intent i = new Intent(Intent.ACTION_PICK);
+        i.setType(ContactsContract.Contacts.CONTENT_TYPE);
+        startActivity(i);
     }
 }
---
+++ PPM_Practica2E2/app/src/main/java/com/simarro/ppm_practica2e2/Utilidades.java
@@ -0,0 +1,29 @@
+package com.simarro.ppm_practica2e2;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import androidx.core.app.ActivityCompat;
+
+public class Utilidades {
+    public Utilidades() {
+    }
+
+    public static void solicitarPermiso(final String permiso, String explicacion, final int codigoDePeticion, final Activity actividad){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(actividad,permiso)){
+            new AlertDialog.Builder(actividad)
+                    .setTitle("Solicitud de permiso")
+                    .setMessage(explicacion)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(actividad,new String[]{permiso},codigoDePeticion);
+                        }
+                    })
+                    .show();
+        }else{
+            ActivityCompat.requestPermissions(actividad,new String[]{permiso},codigoDePeticion);
+        }
+    }
+}
