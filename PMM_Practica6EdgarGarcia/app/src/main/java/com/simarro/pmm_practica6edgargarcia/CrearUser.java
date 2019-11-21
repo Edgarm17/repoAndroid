c690d51f24be13bea2f42c009627859689c99d6b
Edgar <edgaralkugm@gmail.com>
2019-11-21 18:40:53 +0100

    Practica6 #2

---
+++ PMM_Practica6EdgarGarcia/app/src/main/java/com/simarro/pmm_practica6edgargarcia/CrearUser.java
@@ -0,0 +1,14 @@
+package com.simarro.pmm_practica6edgargarcia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_user);
+    }
+}
